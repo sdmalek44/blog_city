f8135109613e931e5f67f7fa81512a7f64876d72
Stephen Malek <malek.stephen44@gmail.com>
2018-07-15 15:29:26 -0600

    add blog show page

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,4 +3,9 @@ class BlogsController < ApplicationController
   def index
     @blogs = Blog.all.includes(:categories)
   end
+
+  def show
+    @blog = Blog.find(params[:id])
+    @categories = @blog.categories
+  end
 end
