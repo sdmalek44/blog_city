bdbcfdc662c2e4e5161e8c2a6eb25f3f686be0c0
Stephen Malek <malek.stephen44@gmail.com>
2018-07-16 15:38:31 -0600

    test user cannot see delete button

--- spec/features/admin/admin_visits_show_page_spec.rb
+++ spec/features/admin/admin_visits_show_page_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe 'admin visiting show page' do
-  it 'can delete a blog' do
+describe 'visiting show page' do
+  it 'admin can delete a blog' do
     admin = User.create(email: 'blah@bla.com', username: "penelope",
                 password: "boom",
                 role: 1)
@@ -18,5 +18,18 @@ describe 'admin visiting show page' do
     expect(current_path).to eq(blogs_path)
     expect(page).to_not have_content(blog1.title, blog1.blurb)
     expect(page).to have_content(blog2.title, blog2.blurb)
+  end
+  it 'default user cannot see delete button' do
+    user = User.create(email: 'blah@bla.com', username: "penelope",
+                password: "boom",
+                role: 0)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    category = Category.create!(name: 'footwear')
+    blog = category.blogs.create!(title: 'shoes', body: 'lookatdeeshoes', image: 'https://i.ytimg.com/vi/AZ2ZPmEfjvU/maxresdefault.jpg')
+
+    visit blog_path(blog)
+
+    expect(page).to_not have_content("Delete")
   end
 end
