98f623bbf1f7541a70c027641f09242a0ff273d4
Stephen Malek <malek.stephen44@gmail.com>
2018-07-16 17:45:38 -0600

    test sad path for blog new

--- spec/features/admin/admin_can_visit_blog_new_page_spec.rb
+++ spec/features/admin/admin_can_visit_blog_new_page_spec.rb
@@ -45,5 +45,22 @@ describe "user visits admin/blogs/new" do
       expect(page).to have_content(cat1.name)
       expect(page).to have_content(cat2.name)
     end
+    it 'goes back to create form if all attributes not entered' do
+      user = User.create!(email: 'blah@bla.com', username: "fern@gully.com", password: "password", role: 1)
+      cat1 = Category.create!(name: 'sports')
+      cat2 = Category.create!(name: 'fitness')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit new_admin_blog_path
+
+      check(cat1.name)
+      check(cat2.name)
+      fill_in :blog_body, with: 'body'
+      fill_in :blog_image, with: 'https://i.ytimg.com/vi/AZ2ZPmEfjvU/maxresdefault.jpg'
+
+      click_on "Create Blog"
+
+      expect(current_path).to eq(admin_blogs_path)
+    end
   end
 end
