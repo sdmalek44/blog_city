c7a869ce99825b90085fa4bf84a733aafdc89b5d
Stephen Malek <malek.stephen44@gmail.com>
2018-07-18 15:11:42 -0600

    test for descend method, fix category/date format

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -24,4 +24,13 @@ body =
       expect(blog.blurb).to eq("#{body[0, 150]}...")
     end
   end
+  describe 'class methods' do
+    it 'can order blogs in descending order by created_at' do
+      blog1 = Blog.create!(title: 'shoes', body: 'lookatdeeshoes', image: 'https://i.ytimg.com/vi/AZ2ZPmEfjvU/maxresdefault.jpg', created_at: Time.new(2015, 1, 13) )
+      blog2 = Blog.create!(title: 'backs', body: 'lookatdeeshoes', image: 'https://i.ytimg.com/vi/AZ2ZPmEfjvU/maxresdefault.jpg', created_at: Time.new(2015, 1, 14) )
+      blog3 = Blog.create!(title: 'shoulders', body: 'lookatdeeshoes', image: 'https://i.ytimg.com/vi/AZ2ZPmEfjvU/maxresdefault.jpg', created_at: Time.new(2015, 1, 15) )
+
+      expect(Blog.descend).to eq([blog3, blog2, blog1])
+    end
+  end
 end
